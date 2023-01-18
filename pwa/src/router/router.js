161c8b0b6b27d517f85a0528155b2c71a41f5dee
zaki-Zakaria <03zaki95@gmail.com>
2023-01-18 17:19:40 +0100

    Create reset-password component and function to send query to the back

--- pwa/src/router/router.js
+++ pwa/src/router/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue';
 import DashboardView from '@/views/DashboardView.vue';
 import AccountVerificationView from '@/views/AccountVerificationView.vue';
+import ResetPassword from '@/views/ResetPasswordView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,8 @@ const router = createRouter({
     { path: '/', name: 'home', redirect: '/login' },
     { path: '/login', name: 'login', component: LoginView, meta: { requiresUnauth: false } },
     { path: '/dashboard', name: 'dashboard', component: DashboardView, meta: { requiresUnauth: true } },
-    { path: '/account-verification/:userId/:token', name: 'account-verification', component: AccountVerificationView , meta: { requiresUnauth: false } }
+    { path: '/account-verification/:userId/:token', name: 'account-verification', component: AccountVerificationView , meta: { requiresUnauth: false } },
+    { path: '/reset-password', name: 'reset-password', component: ResetPassword, meta: { requiresUnauth: false }}
   ]
 })
 
