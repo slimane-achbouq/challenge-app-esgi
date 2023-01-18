4828aa78048174f2ff797b3d84a930bf6427a78f
elmohriothmane <o.elmohri@esi-sba.dz>
2023-01-18 16:07:26 +0100

    fix routes

--- pwa/src/partials/Sidebar.vue
+++ pwa/src/partials/Sidebar.vue
@@ -109,20 +109,14 @@
               </a>
               <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
                 <ul class="pl-9 mt-1" :class="!parentLink.expanded && 'hidden'">
-                  <router-link to="/finance/cards" custom v-slot="{ href, navigate, isExactActive }">
-                    <li class="mb-1 last:mb-0">
-                      <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate" :class="isExactActive && '!text-indigo-500'" :href="href" @click="navigate">
-                        <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Cards</span>
-                      </a>
-                    </li>
-                  </router-link>
-                  <router-link to="/finance/transactions" custom v-slot="{ href, navigate, isExactActive }">
+                  <router-link to="/dashboard/announces" custom v-slot="{ href, navigate, isExactActive }">
                     <li class="mb-1 last:mb-0">
                       <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate" :class="isExactActive && '!text-indigo-500'" :href="href" @click="navigate">
                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Liste des announces</span>
                       </a>
                     </li>
-                  </router-link> 
+                  </router-link>
+                 
                          
                 </ul>
               </div>
