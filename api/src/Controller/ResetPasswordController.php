4966433f267753493208ee80f24da24123419e77
zaki-Zakaria <03zaki95@gmail.com>
2023-01-19 23:31:22 +0100

    create templete for reset-password & some refactoring

--- api/src/Controller/ResetPasswordController.php
+++ api/src/Controller/ResetPasswordController.php
@@ -2,32 +2,51 @@
 
 namespace App\Controller;
 
+use App\Service\JWTService;
 use DateTime;
 use App\Entity\ResetPassword;
 use App\Repository\UserRepository;
 use App\Repository\ResetPasswordRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\SendMailService;
 
 class ResetPasswordController extends AbstractController
 {
+    public function __construct( private SendMailService $sendMailService )
+    {
+    }
+
     #[Route('/reset/password', name: 'app_reset_password', methods:['POST'])]
-    public function reset(Request $request,UserRepository $userRepository,ResetPasswordRepository $resetPasswordRepository): Response
+    public function reset(
+        Request $request,
+        UserRepository $userRepository,
+        ResetPasswordRepository $resetPasswordRepository,
+        ): Response
     {
         $body = json_decode($request->getContent(), true);
         $user = $userRepository->findOneByEmail($body['email']);
+        $token = uniqid();
         if ($user)
         {
             $resetPassword = new ResetPassword();
-            $resetPassword->setToken(uniqid());
+            $resetPassword->setToken($token);
             $resetPassword->setCreatedAt(new DateTime());
             $resetPassword->setUtilisateur($user);
             $resetPasswordRepository->save($resetPassword,true);
 
             //send email here
+            $this->sendMailService->send(
+                'zakaria@gmail.com',
+                $user->getEmail(),
+                'get the link to reset your password',
+                'reset-password',
+                compact('user', 'token')
+            );
         }
 
 
@@ -47,12 +66,12 @@ class ResetPasswordController extends AbstractController
                 $newPassword = $encoder->hashPassword($resetPassword->getUtilisateur() ,$body['newPassword']);
                 $resetPassword->getUtilisateur()->setPassword($newPassword);
                 $resetPasswordRepository->save($resetPassword,true);
-                
+
                 return new Response('new password updated');
             } else {
                 return new Response('there is a wrong password');
             }
-            
+
         }
 
     }
