88a57408e53734774d40a41af1f4f79b922a21c2
Zaid <zaid.anaee@yahoo.com>
2023-02-06 04:01:38 +0100

    create Annonce front

--- pwa/src/partials/Sidebar.vue
+++ pwa/src/partials/Sidebar.vue
@@ -97,7 +97,7 @@
                                             <path class="fill-current text-slate-700" :class=" '!text-indigo-500'" d="M18.007 13c-.474 2.833-2.919 5-5.864 5a5.888 5.888 0 0 1-3.694-1.304L4 20.731C6.131 22.752 8.992 24 12.143 24c6.232 0 11.35-4.851 11.857-11h-5.993Z" />
                                             <path class="fill-current text-slate-600" :class=" 'text-indigo-600'" d="M6.939 15.007A5.861 5.861 0 0 1 6 11.829c0-2.937 2.167-5.376 5-5.85V0C4.85.507 0 5.614 0 11.83c0 2.695.922 5.174 2.456 7.17l4.483-3.993Z" />
                                         </svg>
-                                        <span class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Annouces</span>
+                                        <span class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Announces</span>
                                     </div>
                                     <!-- Icon -->
                                     <div class="flex shrink-0 ml-2">
