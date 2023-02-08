771b055e9d2004e3e5d2d69e5cc01095a40bfbf7
elmohriothmane <o.elmohri@esi-sba.dz>
2023-02-08 17:45:44 +0100

    add search user dashboard

--- pwa/src/partials/dashboard/users/UsersTable.vue
+++ pwa/src/partials/dashboard/users/UsersTable.vue
@@ -1,6 +1,22 @@
 <template>
 
   <div>
+
+    <!-- Search form -->
+    <div class="max-w-xl mb-5">
+                    <form class="relative"      >
+                    <label for="app-search" class="sr-only" >Search</label>
+                    <input id="app-search" class="form-input w-full pl-9 py-3 focus:border-slate-300" type="search" placeholder="Search…" v-model="searchTerm" @input="searchCustomers"/>
+                    <button class="absolute inset-0 right-auto group" type="submit" aria-label="Search">
+                        <svg class="w-4 h-4 shrink-0 fill-current text-slate-400 group-hover:text-slate-500 ml-3 mr-2" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" />
+                        <path d="M15.707 14.293L13.314 11.9a8.019 8.019 0 01-1.414 1.414l2.393 2.393a.997.997 0 001.414 0 .999.999 0 000-1.414z" />
+                        </svg>
+                    </button>
+                    </form>
+    </div>
+
+
     <div class="bg-white shadow-lg rounded-sm border border-slate-200 relative">
       <header class="px-5 py-4">
         <h2 class="font-semibold text-slate-800">All Users <span class="text-slate-400 font-medium">248</span></h2>
@@ -127,10 +143,12 @@
       const selectAll = ref(false)
       const selected = ref([])
       const customers = ref([])
-      const totalResult = ref([0])
-      const lastPage = ref([0])
-      const perPage = ref([2])
-      const page = ref([1])
+      const customersList = ref([])
+      const totalResult = ref(0)
+      const lastPage = ref(0)
+      const perPage = ref(2)
+      const page = ref(1)
+      const searchTerm = ref([])
   
       const checkAll = () => {
         selected.value = []
@@ -156,14 +174,19 @@
           }
         })
         customers.value = response.data["hydra:member"];
+        customersList.value=customers.value
         totalResult.value=response.data["hydra:totalItems"];
         lastPage.value=response.data["hydra:view"]["hydra:last"].split("page=")[1];;
-        console.log(lastPage.value)
-        console.log(totalResult.value)
-        console.log(customers.value)
       } catch (error) {
         console.error(error)
       }
+    }
+
+    const searchCustomers = async() =>  {
+
+      customers.value=customersList.value
+      customers.value=JSON.parse(JSON.stringify(customers.value)).filter(customer =>  customer.email.toLowerCase().includes(searchTerm.value.toLowerCase()))
+      return customersV;
     }
 
     function nextPage() {
@@ -196,7 +219,10 @@
         prevPage,
         lastPage,
         page,
-        totalResult
+        totalResult,
+        searchTerm,
+        searchCustomers,
+        customersList
       }
     }
 
