cc5f5a72bc11ea0e88ab78b3b3ad2373eb7f782b
elmohriothmane <o.elmohri@esi-sba.dz>
2023-02-11 04:52:43 +0100

    handle erros

--- pwa/src/views/UserList.vue
+++ pwa/src/views/UserList.vue
@@ -26,40 +26,62 @@
               </div>
               
               
+              
 
               <!-- Right: Actions  -->
               <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-  
+              
                 <!-- Delete button -->
                 <DeleteButton :selectedItems="selectedItems" @click="modaDeletelOpen=true"/>       
               </div>
   
             </div>
             
-            
+            <Banner type="success" class="mb-4"  :open="true" v-if="updated">
+                    User information updated successfully.
+              </Banner> 
             <!-- Table -->
             <CustomersTable @change-selection="updateSelectedItems($event)" @edit="onOpenModal"/>
             
             <ModalBasic id="feedback-modal" :modalOpen="modalOpen" @close-modal="onModalClose" title="Edit User">
+
+              
                       <!-- Modal content -->
                       <div class="px-5 py-4">
                         <div class="space-y-3 ">
 
+                          <div role="status" v-if="loading">
+                              <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                              </svg>
+                              <span class="sr-only">Loading...</span>
+                            </div>
                          <div class="grid grid-cols-2 gap-4" >
+
+
+                          
                           <div class="col-span-1">
                             <label class="block text-sm font-medium mb-1" for="name">First Name<span class="text-rose-500">*</span></label>
                             <input v-model="selectedItems.firstName" id="name" class="form-input w-full px-2 py-1" type="text" required />
-                          </div>
+                            <div v-if="errors.name" class="text-xs mt-1 text-rose-500">
+                              {{ errors.name }}
+                            </div>
+                            </div>
                           <div class="col-span-1">
                             <label class="block text-sm font-medium mb-1" for="name">Last Name <span class="text-rose-500">*</span></label>
                             <input  v-model="selectedItems.lastName" id="name" class="form-input w-full px-2 py-1" type="text" />
-                          </div>
+                            <div v-if="errors.familyName" class="text-xs mt-1 text-rose-500">
+                            {{ errors.familyName }}
+                            </div>
+                            </div>
                          </div>
 
                          <div class="grid grid-cols-2 gap-4" >
                           <div class="col-span-1">
                             <label class="block text-sm font-medium mb-1" for="name">Mobile <span class="text-rose-500">*</span></label>
                             <input v-model="selectedItems.phoneNumber" id="name" class="form-input w-full px-2 py-1" type="text" required />
+                            <div v-if="error" class="text-xs mt-1 text-rose-500">{{ error }}</div>
                           </div>
                           <div class="col-span-1">
                             <label class="block text-sm font-medium mb-1" for="name">Status <span class="text-rose-500">*</span></label>
@@ -73,7 +95,13 @@
                           <div>
                             <label class="block text-sm font-medium mb-1" for="email">Email <span class="text-rose-500">*</span></label>
                             <input v-model="selectedItems.email" id="email" class="form-input w-full px-2 py-1" type="email" required />
-                          </div>
+                            <div v-if="errors.email" class="text-xs mt-1 text-rose-500">
+                              {{ errors.email }}
+                            </div>
+                            <div v-if="emailExist" class="text-xs mt-1 text-rose-500">
+                              this mail already exists
+                            </div>
+                            </div>
                         </div>
                       </div>
                       <!-- Modal footer -->
@@ -136,6 +164,14 @@
   
   import ModalBasic from '../components/Modal.vue'
   import axios from 'axios'
+  import Banner from "../components/Banner.vue"
+
+  import {
+    phoneValidation,
+    passwordValidation,
+    emailValidation,
+    
+  } from "@/utils/utils-common-function";
 
   
   export default {
@@ -145,7 +181,8 @@
       Header,
       CustomersTable,
       DeleteButton,
-      ModalBasic
+      ModalBasic,
+      Banner
     },
     setup() {
   
@@ -154,6 +191,17 @@
       const modalOpen = ref(false)
       const modaDeletelOpen = ref(false)
       const isVerifiedv=ref(false)
+      const  errors=ref({ name: "",
+                          familyName: "",
+                          phoneNumber: "",
+                          email: "",}) 
+
+      const error=ref(null)
+      const emailExist=ref(false)
+      const loading=ref(false)
+      const updated=ref(false)
+  
+                         
   
       const updateSelectedItems = (selected) => {
         selectedItems.value = selected
@@ -165,12 +213,35 @@
         console.log(selectedItems.value)
       }
 
-      function updateUser(){
+      async function updateUser(){
+        
           
           try {
           // Get the form data from the inputs
-          
-          console.log(isVerifiedv["_value"])
+
+          if (!this.selectedItems.firstName ) {
+              errors.value.name = "Veuillez revérifier votre nom";
+              return;
+            }
+          if (!this.selectedItems.lastName) {
+              errors.value.familyName = "Veuillez revérifier votre prénom";
+              return;
+            }
+
+          if (!emailValidation(this.selectedItems.email)) {
+              console.log("email not")
+              errors.value.email = "Veuillez revérifier votre email s'il est valide";
+              return;
+          }
+
+          if (!phoneValidation(this.selectedItems.phoneNumber)) {
+            console.log("phone not")
+            error.value =
+              "Veuillez revérifier votre numéro de téléphone s'il est valide";
+            return;
+          }
+
+          loading.value = true
           const data = { 
             email: this.selectedItems.email,
             firstName: this.selectedItems.firstName,
@@ -178,16 +249,26 @@
             isVerified: isVerifiedv["_value"] ? true:false,
             phoneNumber: this.selectedItems.phoneNumber  }
           console.log(data)
-          const response = axios.patch(`${import.meta.env.VITE_API_URL}/users/${this.selectedItems.id}`,  data , {
+
+          try {
+          const response = await axios.patch(`${import.meta.env.VITE_API_URL}/users/${this.selectedItems.id}`,  data , {
               headers: {
                   'Authorization': `Bearer ${localStorage.getItem('esgi-ws-token')}`
               }
           })
-          modalOpen.value = false
-          console.log(response)
+        }
+        catch (error) {
+        console.log(error.message);
+        if(error.message="Request failed with status code 500") emailExist.value=true
+        return
+      }
+      loading.value = false
+      updated.value=true
+      modalOpen.value = false
+      
           
       } catch (error) {
-          console.error(error)
+          console.log(error)
       }
           
       
@@ -202,7 +283,12 @@
         onOpenModal,
         modaDeletelOpen,
         updateUser,
-        isVerifiedv
+        isVerifiedv,
+        errors,
+        error,
+        emailExist,
+        loading,
+        updated
       }  
     }
   }
