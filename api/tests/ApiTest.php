aaacb4352da47e865de60a14679810d8b7f396f9
slimane-achbouq <slimane321@gmail.com>
2023-02-11 20:03:52 +0100

    added upload file test scenario WIP

--- api/tests/ApiTest.php
+++ api/tests/ApiTest.php
@@ -3,21 +3,75 @@
 namespace App\Tests;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ApiTest extends ApiTestCase
 {
-    public function testSomething(): void
+    /**
+     * @return mixed
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function testAuthentication()
     {
         $client = static::createClient();
         $authResponse = $client->request(
             'POST',
             '/auth',
-            ['json' => [
-                'email' => 'slimane321@gmail.com',
-                'password' => 'slachslach',
-            ]]);
+            [
+                'json' => [
+                    'email' => 'slimane321@gmail.com',
+                    'password' => 'slachslach',
+                ]
+            ]
+        );
 
         $this->assertResponseIsSuccessful();
-        $this->assertNotNull($authResponse->toArray()['token']);
+        $jwtToken = $authResponse->toArray()['token'];
+        $this->assertNotNull($jwtToken);
+
+        return $jwtToken;
+    }
+
+    /**
+     * @depends testAuthentication
+     * @param string $jwtToken
+     */
+    public function testCreateAnnonce(string $jwtToken)
+    {
+        $client = static::createClient();
+
+        $uploadedFile = new UploadedFile(
+            __DIR__.'/resources/annonce_img.jpg',
+            'annonce_img.jpg'
+        );
+
+        $response = $client->request(
+            'POST',
+            '/annonces',
+            [
+                'headers'           => [
+                    'Content-Type' => 'multipart/form-data',
+                    'Accept' => 'application/json'
+                ],
+                'auth_bearer'       => $jwtToken,
+                'extra' => [
+                    'files' => [
+                        'file' => $uploadedFile,
+                    ],
+                ],
+                'json' => [
+                    'title'         => 'test annonce',
+                    'description'   => 'test description',
+                    'price'         => 20,
+                    'isPerHour'     => true
+                ],
+            ]
+        );
+
+        var_dump($response);die();
     }
 }
